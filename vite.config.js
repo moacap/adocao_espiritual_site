b901d2e526ae66a929ea264baedec4766115afdc
Moacir Alves Pereira <moacir.pereira@gmail.com>
2026-01-13 15:23:58 -0300

    Final fix v4.3 - relative paths

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/adoracao_espiritual_site/',
+  base: './',
   build: {
     rollupOptions: {
       input: {
